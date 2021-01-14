d831667fd42921aa3383f93494739570f156825c
Rodrigo Lima <rodrigodyho@gmail.com>
2021-01-14 01:14:36 -0300

    Add Routes and StepHeader

---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,22 @@
+import Orders from "./Orders";
+import Home from "./Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/orders">
+          <Orders />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
